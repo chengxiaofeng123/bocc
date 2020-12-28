7d86c550a59bdc3a75a2ef660e35ab39128ff5c7
chengxiaofeng <111@qq.com>
2020-12-28 23:55:14 +0800

    sssssss

--- src/components/head.js
+++ src/components/head.js
@@ -9,7 +9,7 @@ export default getQueryString = function(queryName) {
     let reg = new RegExp('(^|&)' + queryName + '=([^&]*)(&|$)', 'i');
     let r = window.location.search.substr(1).match(reg);
     if (r != null) {
-        console.log('sss');
+        console.log('sss2222');
         return decodeURI(r[2]);
     } else {
         return null;
